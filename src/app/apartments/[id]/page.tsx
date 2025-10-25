3ad79bb988ee11ff7a85c3b93406cb5b80d16d07
mindset001 <shittumoshoodademola@gmail.com>
2025-10-25 08:02:36 +0100

    'apartments'

--- src/app/apartments/[id]/page.tsx
+++ src/app/apartments/[id]/page.tsx
@@ -2,13 +2,19 @@ import LandingHeader from "@/components/LandingHeader";
 import LandingFooter from "@/components/LandingFooter";
 import ApartmentDetails from "@/components/ApartmentDetails";
 
-export default function ApartmentDetailsPage({ params }: { params: { id: string } }) {
+export default async function ApartmentDetailsPage({ 
+  params 
+}: { 
+  params: Promise<{ id: string }> 
+}) {
+  const { id } = await params;
+
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
       <LandingHeader />
       <main className="flex-1 flex flex-col items-center pt-20 pb-12 px-4">
         <div className="max-w-4xl w-full">
-          <ApartmentDetails id={params.id} />
+          <ApartmentDetails id={id} />
         </div>
       </main>
       <LandingFooter />
